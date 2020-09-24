e8063d1441091fc78ffa940c72684ad3c05b9abc
Anthony Tagliente <atag3434@gmail.com>
2020-09-24 10:59:19 -0400

    Fix reference to dataframe without outlier filter.

--- R_Geocoding_Census.R
+++ R_Geocoding_Census.R
@@ -1,22 +1,29 @@
 library(dplyr)
 
+## read in trunstile data for getting ids etc
 turns <- read.csv('http://web.mta.info/developers/data/nyct/turnstile/turnstile_200919.txt')
 
-google_key <- "AIzaSyBT2ORkaNi7AoNkLDAHvS9yorsfgiJL8fo"
+#Base url for google maps api
 base <- "https://maps.googleapis.com/maps/api/geocode/json?address="
+
+# Census api key
 cen_key <- '97d55623ee041618526d685d56a340986817ea2c'
 
+# Filter down data to more unique stations
 turns_filt <- turns %>%
   group_by(STATION,DIVISION) %>%
   summarize(all = sum(ENTRIES))
 
+#add empty lat lon columns to fill
 turns_filt$LAT <- NA
 turns_filt$LON <- NA
 
+# iterate through stations and query the api
 for (i in 1:nrow(turns_filt)){
   tryCatch({ 
+    # create an object to store the geo info
     geo <- fromJSON(URLencode(paste(base,paste(turns_filt$STATION[i],"Station",turns_filt$DIVISION[i],'Line New York'),"&key=",key)))
-    # you need to basically make it so the paste puts out whatever address
+
     turns_filt$LON[i] <- geo$results$geometry$location$lng[1] 
     turns_filt$LAT[i]  <-  geo$results$geometry$location$lat[1]
     turns_filt$geo_type[i] <- geo$results$types[[1]]
@@ -24,10 +31,13 @@ for (i in 1:nrow(turns_filt)){
   }, error=function(e){}) 
 }
 
+# create empty columns for census geography ids, these are known as fips and are how you identify the location for
+# pulling in data
 turns_filt$State_FIPS <- NA
 turns_filt$County_FIPS <- NA
 turns_filt$Tract_FIPS <- NA
 
+# run through each row and query the census api to get fips codes based on latitude and longitute coordinates
 for (k in which(is.na(turns_filt$State_FIPS))){
   geo <- fromJSON(paste0('https://geocoding.geo.census.gov/geocoder/geographies/coordinates?x=',turns_filt$LON[k],'&y=',turns_filt$LAT[k],'&benchmark=4&vintage=4'))
   turns_filt$State_FIPS[k] <- geo$result$geographies$`Census Tracts`$STATE
@@ -37,7 +47,7 @@ for (k in which(is.na(turns_filt$State_FIPS))){
 }
 
 
-#Reference Table of Data
+# Then we pull in the data for all tracts in all counties in new york as one query
 
 cen_dat <- fromJSON(paste0('https://api.census.gov/data/2018/acs/acs5/subject?get=NAME,S0101_C05_001E,S0101_C01_001E,S2001_C02_011E,S2001_C02_012E,S2001_C01_002E,S2403_C01_001E,S2403_C01_012E,S2403_C01_017E&for=tract:*&in=state:36&in=county:*&key=',cen_key))
 colnames(cen_dat) <- cen_dat[1,]
